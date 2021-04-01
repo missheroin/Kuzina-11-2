528d73b0530ff09d34ceab6f2461c46ee095d4f9
User <angelinak554@gmail.com>
2021-04-01 11:22:31 +0300

    111

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask import url_for, request, render_template, redirect
 from flask import make_response, session, abort, jsonify
@@ -179,7 +181,8 @@ def main():
     api.add_resource(news_resources.NewsListResource, '/api/v2/news')
     # для одного объекта
     api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run()
 
 
